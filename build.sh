3b25c4713908b991a9828f1fe0049a2a8b896906
Wi-nnie <94724733+Wi-nnie@users.noreply.github.com>
2022-04-15 19:43:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 # Edited for Tecno spark 5 Pro
 
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1"
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0"
 DEVICE=TECNO_CD8
 DT_LINK="https://github.com/Dimonnemo64/TWRP-MT6765-CD8 -b main"
 DT_PATH=device/tecno
