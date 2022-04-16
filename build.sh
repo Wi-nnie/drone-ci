cc6daef7a7003078071407604c1708f8a2f826e9
Wi-nnie <94724733+Wi-nnie@users.noreply.github.com>
2022-04-16 13:09:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -45,7 +45,6 @@ apt-get -qq update \
     xsltproc \
     zip \
     zlib1g-dev \
-    && apt-get purge openjdk-11-jre-headless -y \
     && apt-get autoremove -y \
     && rm -rf /var/lib/apt/lists/*
 curl https://storage.googleapis.com/git-repo-downloads/repo > /bin/repo && chmod a+x /bin/repo
